d3ed818093d67dd689dc53558e0ff549471a8f9c
Kent Brockman <brockman.ks@gmail.com>
2023-02-07 20:29:32 -0700

    merge update_nvim_colors into change_colors

--- files/dotfiles/cmds/change_colors.sh
+++ files/dotfiles/cmds/change_colors.sh
@@ -19,3 +19,36 @@ gsettings set org.gnome.desktop.interface color-scheme $COLORSCHEME
 gsettings set org.gnome.desktop.interface gtk-theme $GTKTHEME
 # update org.gnome.gedit.preferences.editor scheme to ensure the gnome menu has the right item selected
 gsettings set org.gnome.gedit.preferences.editor scheme $SCHEME
+
+# update nvim terminal colors
+# inspired by https://arslan.io/2021/02/15/automatic-dark-mode-for-terminal-applications/
+function send_keys() {
+	# grab all windows for all sessions
+	for wix in $(tmux list-windows -a -F \#S:\#I); do
+		# grab all ttys for panes
+		for pane in $(tmux list-panes -t $wix -F '#P|#{pane_tty}'); do
+			# split $pane into the pane index and tty
+			readarray -d '|' -t splits <<< "$pane";
+			pix="${splits[0]}";
+			ptty="${splits[1]}";
+
+			# grab latest process per tty
+			lastproc=$(ps --tty $ptty -o comm | tail -n1)
+
+			if [[ $lastproc == 'nvim' ]]; then
+				# echo $wix.$pix $lastproc;
+				# start with an escape, end with an enter
+				# Ctrl-V <your key> to record a literal keypress
+				# final : is to clear the message
+				tmux send-keys -t $wix.$pix ':call ChangeBackground():'
+			fi
+		done
+	done
+}
+
+# send keys initially
+send_keys
+
+# watch for changes in system theme via dconf
+# send keys to every nvim instance once a change has been detected
+# dconf watch /org/gnome/desktop/interface/gtk-theme | grep --line-buffered gtk-theme | while IFS='' read -r line; do send_keys; done
